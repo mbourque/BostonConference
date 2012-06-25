9ba099f4bfde43fe5eb8c672ff29cdde6808633a
Michael Bourque <michael.bourque@gmail.com>
2012-06-25 14:09:57 -0400

    Added an unBindModelAll

--- Model/BostonConferenceAppModel.php
+++ Model/BostonConferenceAppModel.php
@@ -9,5 +9,16 @@ class BostonConferenceAppModel extends AppModel {
  */
 	public $actsAs = array( 'Containable' );
 
-}
+	// Unbinds all related models
+	function unbindModelAll() {
+		foreach( array(
+				'hasOne' => array_keys($this->hasOne),
+				'hasMany' => array_keys($this->hasMany),
+				'belongsTo' => array_keys($this->belongsTo),
+				'hasAndBelongsToMany' => array_keys($this->hasAndBelongsToMany)
+		) as $relation => $model) {
+				$this->unbindModel(array($relation => $model));
+		}
+	}
 
+}
